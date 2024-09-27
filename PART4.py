85bad7aaddc11240e95ab96a0aa2f16a5069f2a0
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-09-27 19:44:23 +0300

    part 4 done - just human intervention(human in the loop)

---
+++ PART4.py
@@ -0,0 +1,61 @@
+# Step 1: Import necessary libraries
+from langchain_openai import ChatOpenAI
+from typing import Annotated
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+memory = MemorySaver()
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile(
+    checkpointer=memory,
+    interrupt_before=["tools"],
+)
+
+ascii_graph = graph.get_graph().draw_ascii()
+print(ascii_graph)
+
+user_input = "I'm learning LangGraph. Could you do some research on it for me?"
+config = {"configurable": {"thread_id": "1"}}
+# The config is the **second positional argument** to stream() or invoke()!
+events = graph.stream(
+    {"messages": [("user", user_input)]}, config, stream_mode="values"
+)
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
